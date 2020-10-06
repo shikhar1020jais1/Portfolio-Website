1f5399a890cf8b5b6422d8e739c07a1d80f745f8
Shikhar Jaiswal <shikhar1020jaiswal@gmail.com>
2020-10-06 09:43:51 +0530

    Resumefiles

---
+++ src/resumeData.js
@@ -0,0 +1,143 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Shikhar Jaiswal",
+    "role": "Developer and Programmer",
+    "linkedinId":" shikhar1020jais",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I like dabbling in various parts of Artificial Intelligence and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/shikhar1020jais/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/shikhar1020jais1",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com/_shikhar_jais",
+          "className":"fa fa-twitter"
+        }
+        ,
+        {
+          "name":"Instagram",
+          "url":"http://instagram.com/_shikhar_jais",
+          "className":"fa fa-instagram"
+        },
+        {
+          "name":"Facebook",
+          "url":"http://facebook.com/shikhar.jai1",
+          "className":"fa fa-facebook"
+        }
+        
+      ],
+    "aboutme":"I am currently a student at Presidency University,Bengaluru and pursuing my B.Tech from here. I am a self taught Developer, currently diving deeper into Machine Learning and Artificial Intelligence. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "website":"shikhar1020jaiswal@gmail.com",
+    "address":"India",
+    "education":[
+      {
+        "UniversityName":"Presidency University Bengaluru",
+        "specialization":"Computer Science and Engineering",
+        "MonthOfPassing":"Aug 2018 -",
+        "YearOfPassing":"Aug 2022",
+        "Achievements":"3rd Year"
+      },
+      /*{
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }*/
+    ],
+    "work":[
+      {
+        "CompanyName":"Vyorius Drones LLP",
+        "specialization":"Algorithm Developer Intern",
+        "MonthOfLeaving":"Aug 2020 -",
+        "YearOfLeaving":"Oct 2020",
+        "Achievements":""
+      },
+      /*{
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }*/
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"C "
+      },
+      {
+        "skillname":"cpp"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Django"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"Android Studio"
+      },
+      {
+        "skillname":"React"
+      },
+      {
+        "skillname":"AWS"
+      }
+      ,
+      {
+        "skillname":"CSS"
+      }
+    ],/*
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],*/
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
